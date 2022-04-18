b518614553934499aff639b32001259f0681dfba
RavinduTharaka <44726946+RavinduTharaka@users.noreply.github.com>
2022-04-18 15:23:38 +0530

    Final Version

---
+++ COBSCComp192p-016/ViewModel/CustomPickerViewModel.swift
@@ -0,0 +1,36 @@
+
+import SwiftUI
+import Photos
+
+class CustomPickerViewModel: ObservableObject {
+    @Published var pickerModel = PickerImageModel(data: [], grid: [], disbaledPhotos: false)
+    
+    func getAllImages(){
+        DispatchQueue.global(qos: .background).async {
+            let req = PHAsset.fetchAssets(with: .image, options: .none)
+            req.enumerateObjects { (assest, _, _) in
+                let options = PHImageRequestOptions()
+                options.isSynchronous = true
+                
+                PHCachingImageManager.default().requestImage(for: assest, targetSize: .init(), contentMode: .default, options: options) { (image, _) in
+                    DispatchQueue.main.async {
+                        let data1 = Images(image: image!, selected: false)
+                        self.pickerModel.data.append(data1)
+                    }
+                }
+            }
+            
+            if req.count == self.pickerModel.data.count{
+                self.getGrid()
+            }
+        }
+    }
+    
+    func getGrid(){
+        for i in stride(from: 0, to: self.pickerModel.data.count, by: 3){
+            self.pickerModel.grid.append(i)
+        }
+    }
+
+}
+
